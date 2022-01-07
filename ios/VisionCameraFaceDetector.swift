a3b6b223418fdf863c3c934ec441f01750e0d2cb
Andres GUERRERO <guerrero@unicc.org>
2022-01-07 17:12:25 +0100

    Added luminance to iOS

--- ios/VisionCameraFaceDetector.swift
+++ ios/VisionCameraFaceDetector.swift
@@ -15,7 +15,25 @@ public class FaceDetectorFrameProcessorPlugin: NSObject, FrameProcessorPluginBas
       let context = CIContext(options: nil)
       let cgImage = context.createCGImage(ciimage, from: ciimage.extent)!
       let image = UIImage(cgImage: cgImage)
-
+      
+      
+      
+      var imagePixels = Int(image.size.width*image.size.height*4)
+      var p = 0
+      let provider = cgImage.dataProvider
+      let providerData = provider!.data
+      let data = CFDataGetBytePtr(providerData)
+      var luminance = 0.0
+      while p < imagePixels {
+        let r = CGFloat(data![p]) / 255.0
+        let g = CGFloat(data![p + 1]) / 255.0
+        let b = CGFloat(data![p + 2]) / 255.0
+        luminance += r*0.299 + g*0.587 + b*0.114;
+        p = p + 4
+      }
+      
+      luminance /= image.size.width*image.size.height;
+  
 
       let faceExtractor = FeatureExtractorServiceFactory.serviceWith(type: FeatureExtractorServiceType.MLKit,cropSize: CGFloat(160))
 
@@ -35,9 +53,11 @@ public class FaceDetectorFrameProcessorPlugin: NSObject, FrameProcessorPluginBas
                      Int(features?.bounds.maxY ?? 0)],
           "height": Int(CVPixelBufferGetHeight(imageBuffer)),
           "width": Int(CVPixelBufferGetWidth(imageBuffer)),
-          "trackingId": features?.trackingIDValue as Any
+          "trackingId": features?.trackingIDValue as Any,
+          "luminance": luminance
         ] )
       }
       return result
     }
 }
+
