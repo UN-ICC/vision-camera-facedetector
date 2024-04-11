c09d54fd81b93aab3ceb6e5379333fefc44a272e
Andres GUERRERO <guerrero@unicc.org>
2024-04-11 16:03:16 +0200

    Align with DID version. Parameters not needed anymore

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,10 @@ import { VisionCameraProxy, type Frame } from 'react-native-vision-camera'
 
 const plugin = VisionCameraProxy.initFrameProcessorPlugin('faceDetector')
 
-export interface FaceDetectorConfig{
-  camera?: string;
-  orientation?: string;
-}
-
-export function faceDetector(frame: Frame, config?: FaceDetectorConfig): any {
+export function faceDetector(frame: Frame): any {
   'worklet';
   // @ts-ignore
   if (plugin == undefined) throw new Error('Failed to load Frame Processor Plugin "faceDetector"!')
-  if (config) {
-    let record: Record<string, any> = {};
-    record["camera"] = config.camera;
-    record["orientation"] = config.orientation;
-    return plugin.call(frame, record)
-  }
-  else {
-    return plugin.call(frame)
-  }
 
-
+  return plugin.call(frame)
 }
